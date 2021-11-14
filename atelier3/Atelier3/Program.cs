6f502f86a5a78e509933d56be09303ac1ae8c270
Chanchico <juanfranciscotraversi@gmail.com>
2021-11-14 23:19:21 +0100

    atelier3 fini

--- atelier3/Atelier3/Program.cs
+++ atelier3/Atelier3/Program.cs
@@ -5,8 +5,14 @@ namespace Atelier3
     class Program
     {
         static void Main(string[] args)
-        {
-           
+        {    
+            Transform.Dico();
+            var horaire = new Transform(12, 55);
+            
+            Console.Write($"{Transform.dico[horaire.hour]}, {horaire.minute}");
+        
+            
+               
         }
     }
 }
--- atelier3/Atelier3/Transform.cs
+++ atelier3/Atelier3/Transform.cs
@@ -3,44 +3,57 @@ using System.Collections.Generic;
 
 namespace Atelier3{
     public class Transform{
-     
-        public int Hour;
-        public int Minute;
-        string[] lettre = {
-            "un",           //0
-            "deux",         //1   
-            "trois",        //2     
-            "quatre",       //3    
-            "cinq",         //4
-            "six",          //5
-            "sept",         //6    
-            "huit",         //7   
-            "neuf",         //8
-            "dix",          //9
-            "onze",         //10    
-            "midi",         //11
-            "minuit",       //12
-            "onze",         //13
-            "douze",        //14
-            "treize",       //15
-            "quatorze",     //16
-            "moins quart",  //17
-            "et quart",     //18
-            "seize",        //19
-            "vingt",        //20
-            "trente",       //21
-            "quarante",     //22
-            "cinquante"     //23
-        };
+       public int hour;
+       public int minute;
 
-        public Transform(int hour, int minute){
-            this.Hour = hour;
-            this.Minute = minute;
-            DateTime Hour = new DateTime(1, 1, 1, hour, minute, 0);
-        }
-
+        public static Dictionary<int, string> dico = new Dictionary<int, string>();
+        public static void Dico(){        
+                dico.Add(0, "minuit");
+                dico.Add(1, "une");
+                dico.Add(2, "deux");
+                dico.Add(3, "trois");
+                dico.Add(4, "quatre");
+                dico.Add(5, "cinq");
+                dico.Add(6, "six");
+                dico.Add(7, "sept");
+                dico.Add(8, "huit");
+                dico.Add(9, "neuf");
+                dico.Add(10, "dix");
+                dico.Add(11, "onze");
+                dico.Add(12, "midi");          
+                dico.Add(15, "quart");
+                dico.Add(20, "vingt");
+                dico.Add(25, "vingt-cinq");
+                dico.Add(30 ,"trente");
+             }
         
+        public Transform(int hour, int minute){
+            this.hour = hour;
+            this.minute = minute;
+        }
+        public static string Convert(int hour, int minute){
+            string heure = " heure ";
+            string liant = "";
+            string moment = " du matin";
+            int diff = minute % 5;
 
+            if (hour > 12){
+                hour = hour - 12;
+                moment = " de l'après-midi";
+            }
+            if( hour == 12 || hour == 0){
+                heure = "";
+            }
+            if(minute % 5 != 0){
+                minute -= diff;
+            }
+            if( minute >= 35){
+                hour ++;
+                minute = 60 - minute;
+                liant ="moins ";
+            }
 
-    }
+            return "Il est " + dico[hour] + heure + liant + dico[minute] + moment;
+        }
+    }    
 }
